465d2caf89017785fdfb65c87a0d8547342c098e
Vedika Bopche <vedikabopche2005@gmail.com>
2026-01-19 00:14:53 +0530

    Remove trailing slashes from CORS origins

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://splitstream-frontend.vercel.app/"
+      "https://splitstream-frontend.vercel.app"
     ], 
     methods: ["GET", "POST"]
   }
@@ -21,7 +21,7 @@ const io = new Server(server, {
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://splitstream-frontend.vercel.app/"
+    "https://splitstream-frontend.vercel.app"
   ]
 }));
 
